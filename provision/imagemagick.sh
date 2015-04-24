10f427ceb6f6959b34eb39282831c3a25cd55e00
fffilo <fffilo666@gmail.com>
2015-04-24 13:52:33 +0200

    Provision imagemagick,memcache - check php installation

--- provision/imagemagick.sh
+++ provision/imagemagick.sh
@@ -5,4 +5,11 @@ echo "### imagemagick.sh #############"
 echo "################################"
 
 ### Install imagemagick
-sudo apt-get install -y imagemagick php5-imagick
+sudo apt-get install -y imagemagick
+
+### Install php imagemagick
+php -v > /dev/null 2>&1
+PHP_IS_INSTALLED=$?
+if [[ $PHP_IS_INSTALLED -ne 0 ]]; then
+	sudo apt-get install -y php5-imagick
+fi
--- provision/memcache.sh
+++ provision/memcache.sh
@@ -5,7 +5,14 @@ echo "### memcache.sh ################"
 echo "################################"
 
 ### Install memcached
-sudo apt-get install -y memcached php5-memcache
+sudo apt-get install -y memcached
+
+### Install php memcached
+php -v > /dev/null 2>&1
+PHP_IS_INSTALLED=$?
+if [[ $PHP_IS_INSTALLED -ne 0 ]]; then
+	sudo apt-get install -y php5-memcache
+fi
 
 ### Backup config
 sudo cp /etc/memcached.conf /etc/memcached.conf.default
