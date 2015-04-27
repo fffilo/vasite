0f52e6d4887a1e172cafde073bfcd7d412b367c8
fffilo <fffilo666@gmail.com>
2015-04-27 10:10:40 +0200

    Provision imagemagick php check fix

--- provision/imagemagick.sh
+++ provision/imagemagick.sh
@@ -10,6 +10,6 @@ sudo apt-get install -y imagemagick
 ### Install php imagemagick
 php -v > /dev/null 2>&1
 PHP_IS_INSTALLED=$?
-if [[ $PHP_IS_INSTALLED -ne 0 ]]; then
+if [[ $PHP_IS_INSTALLED -eq 0 ]]; then
 	sudo apt-get install -y php5-imagick
 fi
