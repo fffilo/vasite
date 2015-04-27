7ef0061681c0a8915adc6e81c098face42eef225
fffilo <fffilo666@gmail.com>
2015-04-27 10:26:05 +0200

    Provision memcache php check fix

--- provision/memcache.sh
+++ provision/memcache.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y memcached
 ### Install php memcached
 php -v > /dev/null 2>&1
 PHP_IS_INSTALLED=$?
-if [[ $PHP_IS_INSTALLED -ne 0 ]]; then
+if [[ $PHP_IS_INSTALLED -eq 0 ]]; then
 	sudo apt-get install -y php5-memcache
 fi
 
